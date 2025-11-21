eae6d00b8700a00a9ec7a28af3569f6505b4f66c
RyanC <bluegx08@gmail.com>
2025-11-21 16:38:57 +0800

    Dev jsm (#5)
    * feat: Add hello.js script to log greeting messages.
    
    * feat: Add a new console log "A message" to hello.js
    
    * chore: Add "REVERT Console" Message !
    
    * Revert "feat: Add a new console log "A message" to hello.js"
    
    This reverts commit b52f0cb184619677fad9aa2e9e5f0743a424ad9d.
    
    * save the day
    
    * feat: add comments indicating work-in-progress on new feature implementation
    
    * 修改 hello.js 的內容

---
+++ hello.js
@@ -0,0 +1,11 @@
+console.log("hello Git!");
+console.log("hello GitHub!");
+console.log("I'm Jam");
+
+
+// IMPORTANT: IN THE PROCESS OF ADDING A NEW FEATURE Fix
+
+// I AM IN THE PROCESS OF IMPLEMENING AN IMPORTENT FEATURE
+// CODE
+// CODE
+// CODE
